151ed5a1180b233271ba24af6130f5350edfaf0b
shadowctrl <shadowctrl@shadowctrl.com>
2023-08-05 19:39:55 +0530

    Social Profiles z-index alter

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -6,11 +6,23 @@ import {
   FacebookFilled,
 } from "@ant-design/icons";
 import { SocialIcon } from "react-social-icons";
+import { Link } from "react-router-dom";
 const Navbar = () => {
+  const handleScroll = (id) => {
+    if (window.location.pathname != "/") {
+      window.open(`/${id}`, "_self");
+    }
+
+    setTimeout(() => {
+      document.querySelector(id).scrollIntoView({ behaviour: "smooth" });
+    }, 200);
+  };
+  if (window.location.hash) handleScroll(window.location.hash);
+
   return (
     <div className="parent">
       <div className="components">
-        <div className="title">
+        <div className="title" onClick={() => window.open("/", "_self")}>
           <img src="/images/diamond.png" className="logo" />
           Hackathon
           <div className="socials">
@@ -18,8 +30,8 @@ const Navbar = () => {
               <SocialIcon
                 url="https://www.instagram.com/srmveccsisb"
                 style={{
-                  height: "50px",
-                  width: "50px",
+                  height: "3vw",
+                  width: "3vw",
                 }}
               />
             </div>
@@ -27,8 +39,8 @@ const Navbar = () => {
               <SocialIcon
                 url="https://www.facebook.com"
                 style={{
-                  height: "50px",
-                  width: "50px",
+                  height: "3vw",
+                  width: "3vw",
                 }}
               />
             </div>
@@ -37,8 +49,8 @@ const Navbar = () => {
               <SocialIcon
                 url="https://www.youtube.com/"
                 style={{
-                  height: "50px",
-                  width: "50px",
+                  height: "3vw",
+                  width: "3vw",
                 }}
               />
             </div>
@@ -47,21 +59,24 @@ const Navbar = () => {
 
         <ul>
           <li>
-            <a href="/">Home</a>
+            <p onClick={() => window.open("/", "_self")}>Home</p>
           </li>
           <li>
-            <a href="/schedule">Schedule</a>
+            <p onClick={() => handleScroll("#about")}>About</p>
           </li>
           <li>
-            <a href="/prizes">Prizes</a>
+            <p onClick={() => handleScroll("#schedule")}>Schedule</p>
           </li>
           <li>
-            <a href="/contact">contact</a>
+            <p onClick={() => handleScroll("#contact")}>contact</p>
           </li>
         </ul>
         <div className="last">
           <h2 className="register">
-            <a href="/signin"> Sign In</a>
+            <a href="https://forms.gle/mqHSf1kpoTUiMURH6" target="_blank">
+              {" "}
+              Register
+            </a>
           </h2>
           {/* <h2 className="register">
             <a href="/contact">Contact</a>
