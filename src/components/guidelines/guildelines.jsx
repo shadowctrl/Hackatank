38ef66a46a15c418c406636fa97e08100309ea38
shadowctrl <shadowctrl@shadowctrl.com>
2023-08-05 19:34:46 +0530

    Guidelines for Hackathon

---
+++ src/components/guidelines/guildelines.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import "./guidelines.css";
+
+const guildelines = () => {
+  return (
+    <React.Fragment>
+      <div className="parent-guidelines">
+        <div className="guidelines-title">Guidelines</div>
+        <div className="guidelines-container">
+          <div className="guidelines-content">
+            <div className="guidelines-1">
+              1. Choose teammates with diverse skills,expertise to complement
+              each other
+              <br /> <br />
+              2. create a basic plan, and outline tasks before the hackathon. 📝{" "}
+              <br /> <br />
+              3. Focus on building a functional prototype or MVP within the time
+              limit. 💡 <br /> <br />
+              4. Use Git or similar tools for smooth collaboration in teams. 🔄{" "}
+              <br /> <br />
+              5. Maintain open communication with your team for progress
+              updates. 🗣️
+              <br /> <br /> 6. Set achievable goals and celebrate milestones. 🎯
+              <br />
+              <br /> 7. Utilize APIs, libraries, and open-source tools for
+              efficiency. 🛠️
+              <br /> <br /> 8. Get feedback from mentors or participants to
+              improve your project. 🔄 <br /> <br />
+              9. Regularly test your project to identify and fix bugs. 🧪
+            </div>
+            <div className="guidelines-2">
+              10. Be mindful of time constraints and prioritize tasks. ⏰ <br />{" "}
+              <br />
+              11. Keep notes for presentations and post-hackathon materials. 📑
+              <br /> <br /> 12.Prepare an engaging pitch highlighting your
+              solution's value. 🎤 <br /> <br />
+              13. Be adaptable; consider pivoting if needed. 🔄 <br /> <br />
+              14. Recharge with short breaks, stay hydrated, and eat well. 🥤💤
+              <br /> <br /> 15. Treat everyone with respect, including
+              teammates, mentors, and organizers. 🤝
+              <br /> <br /> 16. Enjoy the experience and connect with
+              like-minded individuals. <br /> <br />
+              🎉 Remember, hackathons are a learning experience, embrace
+              challenges, stay positive, and have fun! 😄
+            </div>
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default guildelines;
