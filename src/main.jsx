489e4b715a4be5ee8f515635295e8e3c8b0b8712
shadowctrl <shadowctrl@shadowctrl.com>
2023-08-05 19:40:56 +0530

    Routes, Components exports

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
-import Hero from "./components/hero/hero";
 import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Error from "./error.jsx";
 import Signin from "./pages/signin.jsx";
 import Dashboard from "./components/dashboard/dashboard.jsx";
-import Navbar from "./components/navbar/navbar";
+import {
+  Navbar,
+  Hero,
+  Events,
+  Guildelines,
+  Software,
+  Hardware,
+  Judges,
+  Aboutus,
+} from "./components";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -17,12 +25,67 @@ ReactDOM.createRoot(document.getElementById("root")).render(
           path="/"
           element={
             <div className="bg">
-              <Navbar /> <Hero />
+              <Navbar />
+              <Hero />
             </div>
           }
         />
         <Route path="/signin" element={<Signin />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route
+          path="/events"
+          element={
+            <div className="events-bg">
+              <Navbar />
+              <Events />
+            </div>
+          }
+        />
+        <Route
+          path="/guidelines"
+          element={
+            <div className="guidelines-bg">
+              <Navbar />
+              <Guildelines />
+            </div>
+          }
+        />
+        <Route
+          path="/software"
+          element={
+            <div className="software-bg">
+              <Navbar />
+              <Software />
+            </div>
+          }
+        />
+        <Route
+          path="/hardware"
+          element={
+            <div className="hardware-bg">
+              <Navbar />
+              <Hardware />
+            </div>
+          }
+        />
+        <Route
+          path="/about"
+          element={
+            <div>
+              <Navbar />
+              <Aboutus />
+            </div>
+          }
+        />
+        <Route
+          path="/judges"
+          element={
+            <div>
+              <Navbar />
+              <Judges />
+            </div>
+          }
+        />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
