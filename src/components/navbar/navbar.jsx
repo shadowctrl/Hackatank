5d5bb44f008211d29b5e232859b0f0323ef6eb32
shadowctrl <shadowctrl@outlook.com>
2023-08-09 06:55:34 +0530

    Navbar Mobile Alignment

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -6,7 +6,8 @@ import {
   FacebookFilled,
 } from "@ant-design/icons";
 import { SocialIcon } from "react-social-icons";
-import { Link } from "react-router-dom";
+import { ReactComponent as Grid } from "../../assets/navgrid.svg";
+
 const Navbar = () => {
   const handleScroll = (id) => {
     if (window.location.pathname != "/") {
@@ -19,74 +20,116 @@ const Navbar = () => {
   };
   if (window.location.hash) handleScroll(window.location.hash);
 
+  const handleNavMenuClick = () => {
+    const NavVisiblility = document.querySelector(".parent-navbar-mobile");
+    NavVisiblility.classList.toggle("navbar-open-mobile");
+  };
   return (
-    <div className="parent">
-      <div className="components">
-        <div className="title" onClick={() => window.open("/", "_self")}>
-          <img src="/images/diamond.webp" className="logo" />
-          Hackathon
-          <div className="socials">
-            <div className="insta">
-              <SocialIcon
-                url="https://www.instagram.com/srmveccsisb"
-                style={{
-                  height: "3vw",
-                  width: "3vw",
-                }}
-              />
-            </div>
-            <div className="fb">
-              <SocialIcon
-                url="https://www.facebook.com"
-                style={{
-                  height: "3vw",
-                  width: "3vw",
-                }}
-              />
-            </div>
+    <React.Fragment>
+      <div className="parent">
+        <div className="components">
+          <div className="title" onClick={() => window.open("/", "_self")}>
+            <img src="/images/diamond.webp" className="logo" />
+            Hackathon
+            <div className="socials">
+              <div className="insta">
+                <SocialIcon
+                  url="https://www.instagram.com/srmveccsisb"
+                  style={{
+                    height: "3vw",
+                    width: "3vw",
+                  }}
+                />
+              </div>
+              <div className="fb">
+                <SocialIcon
+                  url="https://www.facebook.com"
+                  style={{
+                    height: "3vw",
+                    width: "3vw",
+                  }}
+                />
+              </div>
 
-            <div className="yt">
-              <SocialIcon
-                url="https://www.youtube.com/"
-                style={{
-                  height: "3vw",
-                  width: "3vw",
-                }}
-              />
+              <div className="yt">
+                <SocialIcon
+                  url="https://www.youtube.com/"
+                  style={{
+                    height: "3vw",
+                    width: "3vw",
+                  }}
+                />
+              </div>
             </div>
           </div>
-        </div>
 
-        <ul>
-          <li>
-            <p onClick={() => window.open("/", "_self")}>Home</p>
-          </li>
-          <li>
-            <p onClick={() => handleScroll("#agenda")}>Agenda</p>
-          </li>
-          <li>
-            <p onClick={() => handleScroll("#schedule")}>Schedule</p>
-          </li>
-          <li>
-            <p onClick={() => handleScroll("#faqs")}>FAQs</p>
-          </li>
-          <li>
-            <p onClick={() => handleScroll("#contact")}>Contact</p>
-          </li>
-        </ul>
-        <div className="last">
-          <h2 className="register">
-            <a href="https://forms.gle/mqHSf1kpoTUiMURH6" target="_blank">
-              {" "}
-              Register
-            </a>
-          </h2>
-          {/* <h2 className="register">
-            <a href="/contact">Contact</a>
-          </h2> */}
+          <ul>
+            <li>
+              <p onClick={() => window.open("/", "_self")}>Home</p>
+            </li>
+            <li>
+              <p onClick={() => handleScroll("#agenda")}>Agenda</p>
+            </li>
+            <li>
+              <p onClick={() => handleScroll("#schedule")}>Schedule</p>
+            </li>
+            <li>
+              <p onClick={() => handleScroll("#faqs")}>FAQs</p>
+            </li>
+            <li>
+              <p onClick={() => handleScroll("#contact")}>Contact</p>
+            </li>
+          </ul>
+          <div className="last">
+            <h2 className="register">
+              <a href="https://forms.gle/mqHSf1kpoTUiMURH6" target="_blank">
+                {" "}
+                Register
+              </a>
+            </h2>
+          </div>
         </div>
       </div>
-    </div>
+
+      <div className="parent-navbar-mobile">
+        <div className="components-mobile">
+          <div className="title-mobile">
+            <img src="/images/diamond.webp" width="30vw" />
+            Hackathon
+          </div>
+          <div className="last-mobile">
+            <h2 className="register-mobile">
+              <a href="https://forms.gle/mqHSf1kpoTUiMURH6" target="_blank">
+                Register
+              </a>
+            </h2>
+          </div>
+          <Grid
+            className="navbar-menu-mobile "
+            onClick={() => handleNavMenuClick()}
+          />
+        </div>
+        <div className="navbar-open">
+          <ul>
+            <li>
+              <p onClick={() => window.open("/", "_self")}>Home</p>
+            </li>
+            <li>
+              <p onClick={() => handleScroll("#agenda")}>Agenda</p>
+            </li>
+            <li>
+              <p onClick={() => handleScroll("#schedule")}>Schedule</p>
+            </li>
+            <li>
+              <p onClick={() => handleScroll("#faqs")}>FAQs</p>
+            </li>
+            <li>
+              <p onClick={() => handleScroll("#contact")}>Contact</p>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </React.Fragment>
   );
 };
 
