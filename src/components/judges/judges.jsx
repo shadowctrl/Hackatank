2ff014fb33dbef0abb3885770ef80c6c50b3434e
ThePythonGuyy <thepythonguy@outlook.com>
2023-09-04 11:32:58 +0530

    judge panel1.0

--- src/components/judges/judges.jsx
+++ src/components/judges/judges.jsx
@@ -1,13 +1,31 @@
 import React, { useEffect } from "react";
 import "./judges.css";
-
+import Judge3 from "./Judge3";
+import { set1, set2, set3, set4 } from "./Details";
 const judges = () => {
   useEffect(() => {
     document.title = "Judge Panel | Hack a Tank";
-  });
+  },[]);
+
+
   return (
     <div className="parent-judges">
-      <h1 className="judges-title">Judges Yet To Be Decided</h1>
+      <section className="bg-judges">
+        
+            <img src="/images/judgeP/bg.png" alt="hjhg" className="bg_img" />
+        
+        <div className="bg_img-overlay"></div>
+
+      </section>
+      <div className="title">
+        <h1>judge panel</h1>
+      </div>
+      <section className="judges">
+      <Judge3 mem = {set1} />
+      <Judge3 mem = {set2} />
+      <Judge3 mem = {set3} />
+      <Judge3 mem = {set4} />
+      </section>
     </div>
   );
 };
