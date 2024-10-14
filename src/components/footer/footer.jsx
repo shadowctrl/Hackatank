f4c2f4be43c253b5faa2d972f0f6707188e90a57
Shadowctrl <shadowctrl@outlook.com>
2024-10-14 16:34:52 +0530

    "Updated footer component: changed CSS styles for copyright links, updated copyright year, and modified developed by link to open two URLs on click."

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -2,19 +2,20 @@ import React from "react";
 import "./footer.css";
 import { ReactComponent as Copyright } from "../../assets/copyright.svg";
 const footer = () => {
+  const developedBy = () => {
+    window.open("https://shadowctrl.me", "_blank");
+    window.open("https://upwork.com/freelancers/shadowctrl", "_blank");
+  };
   return (
     <React.Fragment>
       <div className="parent-footer">
         <p className="copyright">
           Copyrights
           <Copyright className="copyright-icon" />
-          2023 - {"All Rights Reserved."}
+          2024-2025 - {"All Rights Reserved."}
         </p>
         <p className="copyright">
-          Developed by{" "}
-          <a href="https://upwork.com/freelancers/shadowctrl" target="_blank">
-            shadowctrl
-          </a>
+          Developed & Maintained by <p onClick={developedBy}>shadowctrl</p>
         </p>
       </div>
     </React.Fragment>
